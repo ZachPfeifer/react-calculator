cdbcbe8a3bc74ef563ede2eb12e97fcec50dd972
Zach Pfeifer <mr.zman1@yahoo.com>
2019-11-13 11:57:50 -0700

    Button done?

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 //Main placeholder component
 
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import update from 'immutability-helper'
+// import math from 'mathjs'
+import './App.css'
+// import Display from './Display.js'
+// import Button from './Button'
+// import Buttons from './Buttons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { operations: [] }
+  }
+
+
 }
+
 
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,1 +1,34 @@
 //individual button
+
+render(){
+  return (
+    <div className="App">
+      <Display data={this.StaticRange.operations} />
+      <Buttons>
+        <Button onClick={this.handleClick} label="C" value="clear" />
+        <Button onClick={this.handleClick} label="7" value="7" />
+        <Button onClick={this.handleClick} label="4" value="4" />
+        <Button onClick={this.handleClick} label="1" value="1" />
+
+        <Button onClick={this.handleClick} label="/" value="/" />
+        <Button onClick={this.handleClick} label="8" value="8" />
+        <Button onClick={this.handleClick} label="5" value="5" />
+        <Button onClick={this.handleClick} label="2" value="2" />
+        <Button onClick={this.handleClick} label="." value="." />
+
+
+        <Button onClick={this.handleClick} label="X" value="*" />
+        <Button onClick={this.handleClick} label="9" value="9" />
+        <Button onClick={this.handleClick} label="6" value="6" />
+        <Button onClick={this.handleClick} label="3" value="3" />
+        <Button label="" value="null" />
+
+        <Button onClick={this.handleClick} label="-" value="-" />
+        <Button onClick={this.handleClick} label="+" value="+" size="2" />
+        <Button onClick={this.handleClick} label="=" value="equal" size="2" />
+
+
+      </Buttons>
+    </div>
+  )
+}
